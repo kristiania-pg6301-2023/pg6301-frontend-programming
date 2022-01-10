df40143671d7366cc2aef4b419f62201adfa3ee4
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-10 09:18:33 +0100

    List answer alternatives

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,12 @@ import {randomQuestion} from "./questions";
 function Quiz() {
     const [question, setQuestion] = useState(randomQuestion());
 
-    return <h1>{question.question}</h1>;
+    return <>
+        <h1>{question.question}</h1>
+        {Object.keys(question.answers)
+            .filter(a => question.answers[a])
+            .map(a => <p key={a}><button>{question.answers[a]}</button></p>)}
+    </>;
 }
 
 ReactDOM.render(<Quiz/>, document.getElementById("app"));
