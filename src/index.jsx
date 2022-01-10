fb2a864dfe1fe25492bb5f0494d2175cc90dd761
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-10 09:20:03 +0100

    Handle button onClick

--- src/index.jsx
+++ src/index.jsx
@@ -6,12 +6,19 @@ import {randomQuestion} from "./questions";
 
 function Quiz() {
     const [question, setQuestion] = useState(randomQuestion());
+    const [answer, setAnswer] = useState();
+
+    if (answer) {
+        return <h1>Right or wrong</h1>;
+    }
 
     return <>
         <h1>{question.question}</h1>
         {Object.keys(question.answers)
             .filter(a => question.answers[a])
-            .map(a => <p key={a}><button>{question.answers[a]}</button></p>)}
+            .map(a => <p key={a}>
+                <button onClick={e => setAnswer(a)}>{question.answers[a]}</button>
+            </p>)}
     </>;
 }
 
