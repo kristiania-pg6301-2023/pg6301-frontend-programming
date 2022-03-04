91a8c9804e39f97a41149653551aad1fb2ef7b71
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-03-04 13:54:02 +0100

    React app for Heroku

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.static("../client/dist/"));
+
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address().port}`);
 });
