782991109c54710a0552c6335908760daffc2e5d
Johannes Brodwall <johannes@brodwall.com>
2023-08-21 19:47:26 +0200

    handle post on server

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 import express from "express";
 import path from "path";
+import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.json());
+
+const movies = [
+    {title: "Oppenheimer", year: "2023", plot: "Boom!"},
+    {title: "Barbie", year: "2023", plot: "Plastic"}
+];
+
 app.get("/api/movies", (req, res) => {
-    res.json([
-        {title: "Oppenheimer", year: "2023", plot: "Boom!"},
-        {title: "Barbie", year: "2023", plot: "Plastic"}
-    ])
+    res.json(movies)
 });
+
+app.post("/api/movies", (req, res) => {
+    const {title, year, plot} = req.body;
+    movies.push({title, year, plot});
+    res.sendStatus(204);
+})
 
 app.use(express.static("../client/dist"));
 app.use((req, res, next) => {
