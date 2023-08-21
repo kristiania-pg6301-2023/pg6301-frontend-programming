ca4fa0c744db3e5b108e9aeb694b95f06727c7d8
Johannes Brodwall <johannes@brodwall.com>
2023-08-21 19:26:03 +0200

    serve client code and api

--- server/server.js
+++ server/server.js
@@ -2,4 +2,12 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/movies", (req, res) => {
+    res.json([
+        {title: "Oppenheimer"},
+    ])
+})
+
+app.use(express.static("../client/dist"));
+
 app.listen(3000);
