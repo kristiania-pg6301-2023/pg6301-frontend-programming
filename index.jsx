62aa9b26cc1399b7c4ee0418f6c0ec4414556c40
Johannes Brodwall <johannes@brodwall.com>
2023-08-23 13:38:20 +0200

    Application layout and CSS

--- index.jsx
+++ index.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import "./application.css"
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Movie database</h1>);
+
+function Application() {
+    return <>
+        <header><h1>Movie Application</h1></header>
+        <nav><a href={"#"}>Home page</a></nav>
+        <main>
+            <h2>Welcome to the front page</h2>
+        </main>
+        <footer>
+            By Johannes Brodwall for Kristiania 2023
+        </footer>
+    </>;
+}
+
+root.render(<Application />);
