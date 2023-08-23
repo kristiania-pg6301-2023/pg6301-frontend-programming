7275b6a26fc08e08250c0e383d4ccc79fff03014
Johannes Brodwall <johannes@brodwall.com>
2023-08-23 14:10:26 +0200

    Very simple API and restart

--- server/server.js
+++ server/server.js
@@ -2,5 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/movies", (req, res) => {
+    res.json([
+        { title: "Barbie", year: "2023" },
+        { title: "Oppenheimer", year: "2023" },
+        { title: "Bad year", year: "2023" },
+    ])
+})
+
+
 app.listen(3000);
 
