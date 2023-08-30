d0fa0756ad6536961472fd6f4a568578e599be69
Johannes Brodwall <johannes@brodwall.com>
2023-08-30 12:58:53 +0200

    Initial application structure with FrontPage, ListMovies and AddMovie

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello class</h1>)
+
+function FrontPage() {
+    return <>
+        <h2>Welcome to the application</h2>
+        <ul>
+            <li><a href="#">List the movies</a></li>
+            <li><a href="#">Add a movie</a></li>
+        </ul>
+    </>;
+}
+
+function ListMovies() {
+    return <h2>List movies</h2>;
+}
+
+function NewMovie() {
+    return <h2>Add a movie to the database</h2>;
+}
+
+function MovieApplication() {
+    return <>
+        <header>
+            <h1>Movie Application</h1>
+        </header>
+        <nav><a href="#">Front page</a></nav>
+        <main>
+            <ListMovies />
+        </main>
+        <footer>Created by Johannes Brodwall 💚</footer>
+    </>;
+}
+
+root.render(<MovieApplication/>)
