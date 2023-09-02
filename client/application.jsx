60072ed1adbbf479007c9ed89bea86287cd881bf
Johannes Brodwall <johannes@brodwall.com>
2023-09-02 23:47:27 +0200

    client tries to fetch tasks, but gets CORS error

--- client/application.jsx
+++ client/application.jsx
@@ -1,5 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+
+function ListTasks() {
+    const [tasks, setTasks] = useState(undefined);
+
+    useEffect(async () => {
+        const res = await fetch("http://localhost:3000/api/todos");
+        setTasks(await res.json());
+    }, [])
+
+    if (!tasks) {
+        return <div>Loading ...</div>;
+    }
+
+    return <>
+        <h2>Tasks</h2>
+        {tasks.map(t => <div>{t.title}</div>)}
+        </>;
+}
 
 export function TodoApplication() {
-    return <h1>Welcome to the task application</h1>;
+    return <>
+        <h1>Welcome to the task application</h1>
+
+        <ListTasks />
+
+        </>;
 }
