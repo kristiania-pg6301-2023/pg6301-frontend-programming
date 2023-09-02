4edc69008bf521b7f52c0ca5221a623cf6fd5aa1
Johannes Brodwall <johannes@brodwall.com>
2023-09-02 23:36:58 +0200

    Simple server functionality

--- server/server.js
+++ server/server.js
@@ -2,4 +2,14 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/todos", (req, res) => {
+    res.json([
+        { title: "prepare lecture" },
+        { title: "give lecture" },
+    ])
+})
+
+
+app.use(express.static("public"));
+
 app.listen(3000);
