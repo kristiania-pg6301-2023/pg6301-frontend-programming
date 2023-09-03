a0f19c93b388e9c15defaf3fd5554962bdb73862
Johannes Brodwall <johannes@brodwall.com>
2023-09-03 20:53:38 +0200

    Add task functionality

--- client/index.jsx
+++ client/index.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom/client";
-import {HashRouter, NavLink, Route, Routes} from "react-router-dom";
+import {HashRouter, Link, NavLink, Route, Routes, useNavigate} from "react-router-dom";
 
 import "./application.css";
 
@@ -22,14 +22,43 @@ function FrontPage() {
 
     return <>
         <h2>Front page</h2>
+        <div><Link to={"/tasks/new"}>Add task</Link></div>
         {tasks.map(task => <div key={task.title}><h3>{task.title}</h3></div>)}
     </>;
+}
+
+function AddTask() {
+    const [title, setTitle] = useState("");
+    const navigate = useNavigate();
+
+    async function handleSubmit() {
+        const res = await fetch("/api/tasks", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify({title})
+        });
+        navigate("/");
+    }
+
+
+    return <form method={"dialog"} onSubmit={handleSubmit}>
+        <h2>Add task</h2>
+        <div>
+            Title:<br />
+            <input name="title" autoFocus={true} value={title} onChange={e => setTitle(e.target.value)}/>
+        </div>
+        <button>
+            Submit
+        </button>
+    </form>;
 }
 
 function TasksRoutes() {
     return <Routes>
         <Route path={"/"} element={<FrontPage/>}/>
-        <Route path={"/tasks/new"} element={<h2>Add task</h2>}/>
+        <Route path={"/tasks/new"} element={<AddTask/>}/>
         <Route path={"*"} element={<h2>Not found</h2>}/>
     </Routes>;
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 app.use(express.static("../client/dist"));
 
 const tasks = [
@@ -22,5 +24,10 @@ app.get("/api/tasks", (req, res) => {
     res.json(tasks);
 });
 
+app.post("/api/tasks", (req, res) => {
+    const {title} = req.body;
+    tasks.push({title, state: "todo"});
+    res.send();
+});
 
 app.listen(process.env.PORT || 3000);
