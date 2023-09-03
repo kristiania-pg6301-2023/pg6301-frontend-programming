e002867bc8c854eb31db7f2d99e5444f3bacc82e
Johannes Brodwall <johannes@brodwall.com>
2023-09-03 19:48:20 +0200

    Create application structure

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {HashRouter, NavLink, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello Movies</h1>)
+
+function TasksRoutes() {
+    return <Routes>
+        <Route path={"/"} element={<h2>Front page</h2>}/>
+        <Route path={"/tasks/new"} element={<h2>Add task</h2>}/>
+        <Route path={"*"} element={<h2>Not found</h2>}/>
+    </Routes>;
+}
+
+function TaskApplication() {
+    return <HashRouter>
+        <header><h1>Task application</h1></header>
+        <nav><NavLink to={"/"}>Front page</NavLink></nav>
+        <main>
+            <TasksRoutes/>
+        </main>
+        <footer>Create by Johannes @ Kristiania</footer>
+    </HashRouter>;
+}
+
+root.render(<TaskApplication/>)
