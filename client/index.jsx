937f9a5f4acb651631a678afda76ec28d36d5e1c
Johannes Brodwall <johannes@brodwall.com>
2023-09-03 19:54:37 +0200

    Front page

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom/client";
 import {HashRouter, NavLink, Route, Routes} from "react-router-dom";
 
@@ -7,9 +7,33 @@ import "./application.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+function FrontPage() {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        setTasks([
+            {
+                title: "Prepare lecture", state: "done"
+            },
+            {
+                title: "Do lecture", state: "doing"
+            },
+            {
+                title: "Finish lecture", state: "todo"
+            }
+        ])
+    }, []);
+
+
+    return <>
+        <h2>Front page</h2>
+        {tasks.map(task => <div key={task.title}><h3>{task.title}</h3></div>)}
+    </>;
+}
+
 function TasksRoutes() {
     return <Routes>
-        <Route path={"/"} element={<h2>Front page</h2>}/>
+        <Route path={"/"} element={<FrontPage/>}/>
         <Route path={"/tasks/new"} element={<h2>Add task</h2>}/>
         <Route path={"*"} element={<h2>Not found</h2>}/>
     </Routes>;
