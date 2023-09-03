5b2083bbb214e07745984cfe74942cc29d80f78c
Johannes Brodwall <johannes@brodwall.com>
2023-09-03 22:18:42 +0200

    Add prettier  check and run prettier

--- client/index.jsx
+++ client/index.jsx
@@ -1,50 +1,69 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom/client";
-import {BrowserRouter, NavLink, Route, Routes} from "react-router-dom";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 
 import "./application.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function ListMovies() {
-    const [movies, setMovies] = useState([]);
-    async function fetchMovies() {
-        const res = await fetch("/api/movies");
-        setMovies(await res.json());
-    }
-    useEffect(() => {
-        fetchMovies();
-    }, []);
+  const [movies, setMovies] = useState([]);
+  async function fetchMovies() {
+    const res = await fetch("/api/movies");
+    setMovies(await res.json());
+  }
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
-    return <>
-        <h2>All movies</h2>
-        {movies.map(m => <div key={m.title}><h3>{m.title}</h3></div>)}
-        </>;
+  return (
+    <>
+      <h2>All movies</h2>
+      {movies.map((m) => (
+        <div key={m.title}>
+          <h3>{m.title}</h3>
+        </div>
+      ))}
+    </>
+  );
 }
 
 function AddMovie() {
-    return <>
-        <h2>Movies</h2>
-    </>;
+  return (
+    <>
+      <h2>Movies</h2>
+    </>
+  );
 }
 
 function MoviesRoutes() {
-    return <Routes>
-        <Route path={"/"} element={<ListMovies />} />
-        <Route path={"/movies/new"} element={<AddMovie />} />
-        <Route path={"*"} element={<h2>Not Found</h2>} />
-    </Routes>;
+  return (
+    <Routes>
+      <Route path={"/"} element={<ListMovies />} />
+      <Route path={"/movies/new"} element={<AddMovie />} />
+      <Route path={"*"} element={<h2>Not Found</h2>} />
+    </Routes>
+  );
 }
 
 function MoviesApplication() {
-    return <BrowserRouter>
-        <header><h1>Movies Database</h1></header>
-        <nav><NavLink to={"/"}>List movies</NavLink><NavLink to={"/movies/new"}>Add movie</NavLink><div className={"spacer"} /><NavLink to={"/login"}>User profile</NavLink></nav>
-        <main>
-            <MoviesRoutes />
-        </main>
-        <footer>Made with 💚 by Johannes</footer>
-    </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>Movies Database</h1>
+      </header>
+      <nav>
+        <NavLink to={"/"}>List movies</NavLink>
+        <NavLink to={"/movies/new"}>Add movie</NavLink>
+        <div className={"spacer"} />
+        <NavLink to={"/login"}>User profile</NavLink>
+      </nav>
+      <main>
+        <MoviesRoutes />
+      </main>
+      <footer>Made with 💚 by Johannes</footer>
+    </BrowserRouter>
+  );
 }
 
-root.render(<MoviesApplication/>)
+root.render(<MoviesApplication />);
--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ import express from "express";
 
 const app = express();
 app.get("/api/movies", (req, res) => {
-    res.json([
-        {title: "Barbie", year: "2023", plot: "Adventures in Pink"},
-        {title: "Oppenheimer", year: "2023", plot: "The Manhattan project"}
-    ])
-})
+  res.json([
+    { title: "Barbie", year: "2023", plot: "Adventures in Pink" },
+    { title: "Oppenheimer", year: "2023", plot: "The Manhattan project" },
+  ]);
+});
 
 app.use(express.static("../client/dist"));
 
