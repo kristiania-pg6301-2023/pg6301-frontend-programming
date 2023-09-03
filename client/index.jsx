6083acab0a1f5ef00ad04c250c8fc13083a3bc10
Johannes Brodwall <johannes@brodwall.com>
2023-09-03 21:49:27 +0200

    Movies application structure

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello React</h1>)
+
+function ListMovies() {
+    return <>
+        <h2>Movies</h2>
+        </>;
+}
+
+function AddMovie() {
+    return <>
+        <h2>Movies</h2>
+    </>;
+}
+
+function MoviesRoutes() {
+    return <Routes>
+        <Route path={"/"} element={<ListMovies />} />
+        <Route path={"/movies/new"} element={<AddMovie />} />
+        <Route path={"*"} element={<h2>Not Found</h2>} />
+    </Routes>;
+}
+
+function MoviesApplication() {
+    return <BrowserRouter>
+        <header><h1>Movies Database</h1></header>
+        <nav><Link to={"/"}>List movies</Link><Link to={"/movies/new"}>Add movie</Link><div className={"spacer"} /><Link to={"/login"}>User profile</Link></nav>
+        <main>
+            <MoviesRoutes />
+        </main>
+        <footer>Made with 💚 by Johannes</footer>
+    </BrowserRouter>;
+}
+
+root.render(<MoviesApplication/>)
