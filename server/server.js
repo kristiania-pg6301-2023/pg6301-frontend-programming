62ea1e016e1579cf5a1e2f7b4a2dc47a1f5af0a3
Johannes Brodwall <johannes@brodwall.com>
2023-09-03 16:46:13 +0200

    Implement (incorrectly /api/questions/answer)

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {randomQuestion} from "./questions.js";
+import {isCorrectAnswer, randomQuestion, Questions} from "./questions.js";
 
 const app = express();
 
@@ -8,6 +8,13 @@ app.use(express.static("../client/dist"));
 app.get("/api/questions/random", (req, res) => {
     const {id, question, answers} = randomQuestion();
     res.json({id, question, answers})
+})
+
+app.post("/api/questions/answer", (req, res) => {
+    const {id, answer} = req.body();
+    const question = Questions.find(q => q.id === id);
+    const correct = isCorrectAnswer(question, answer);
+    res.json({correct})
 })
 
 
