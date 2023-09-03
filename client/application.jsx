dbf85c209fd1069294e2d07ab945a5da402c434a
Johannes Brodwall <johannes@brodwall.com>
2023-09-03 15:03:30 +0200

    Show AddTaskButton and dialog

--- client/application.jsx
+++ client/application.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 
 function ListTasks() {
     const [tasks, setTasks] = useState(undefined);
@@ -8,6 +8,7 @@ function ListTasks() {
             const res = await fetch("/api/todos");
             setTasks(await res.json());
         }
+
         fetchData();
     }, [])
 
@@ -16,16 +17,52 @@ function ListTasks() {
     }
 
     return <>
-        <h2>Tasks</h2>
         {tasks.map(t => <div key={t.title}>{t.title}</div>)}
-        </>;
+    </>;
 }
+
+function AddTaskButton() {
+    const dialogRef = useRef();
+    const [showDialog, setShowDialog] = useState(false);
+    useEffect(() => {
+        if (showDialog) {
+            dialogRef.current.showModal();
+        } else {
+            dialogRef.current.close();
+        }
+    }, [showDialog]);
+    const [title, setTitle] = useState("");
+
+    async function handleSubmit() {
+
+    }
+
+    return <>
+        <dialog ref={dialogRef}>
+            <h2>Add task</h2>
+            <form onSubmit={handleSubmit} method={"dialog"}>
+                <div>
+                    Title <br />
+                    <input autoFocus={true} value={title} onChange={e => setTitle(e.target.value)}/>
+                </div>
+                <div>
+                    <button>Submit</button>
+                    <button onClick={() => setShowDialog(false)}>Close</button>
+                </div>
+            </form>
+        </dialog>
+        <button onClick={() => setShowDialog(true)}>Add task</button>
+    </>;
+}
+
 
 export function TodoApplication() {
     return <>
         <h1>Welcome to task application</h1>
 
-        <ListTasks />
+        <h2>Tasks</h2>
+        <AddTaskButton/>
+        <ListTasks/>
 
-        </>;
+    </>;
 }
