9aefa9b48d4f3787a903384d8f43dc14575086db
Johannes Brodwall <johannes@brodwall.com>
2023-09-05 14:47:01 +0200

    add server API

--- server/server.js
+++ server/server.js
@@ -2,4 +2,18 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/todos", (req, res) => {
+    res.send([
+        {
+            title: "implement server",
+            status: "doing"
+        },
+        {
+            title: "first version of client",
+            status: "done"
+        }
+    ]);
+});
+
+
 app.listen(3000);
