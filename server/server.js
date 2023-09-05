346068440bad684da4e5cf6c4d40115fb415a9d3
Johannes Brodwall <johannes@brodwall.com>
2023-09-05 15:47:43 +0200

    cleanup superfluous logging

--- server/server.js
+++ server/server.js
@@ -3,11 +3,6 @@ import bodyParser from "body-parser";
 
 const app = express();
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    console.log("Request logging by johannes", req.url);
-    req.brukernavn = "pg6301-student"
-    next();
-})
 
 const tasks = [
     {
@@ -25,12 +20,11 @@ const tasks = [
 ]
 
 app.get("/api/todos", (req, res) => {
-    console.log(req.brukernavn);
     res.send(tasks);
 });
+
 app.post("/api/todos", (req, res) => {
     const {title} = req.body;
-    console.log("body", req.body);
     const newTask = {title, status: "todo"};
     tasks.push(newTask);
     res.send();
