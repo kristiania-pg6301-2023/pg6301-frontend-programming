ec55a8b837431ba3deb92c465a72fe0b8c706d59
Johannes Brodwall <johannes@brodwall.com>
2023-09-05 14:39:58 +0200

    Task application with two sample tasks

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello React</h1>)
+
+function TaskApplication() {
+    const [tasks, setTasks] = useState();
+    useEffect(() => {
+        setTasks([
+            {
+                title: "Prepare lecture",
+                status: "done"
+            },
+            {
+                title: "Give lecture",
+                status: "doing"
+            }
+        ])
+    }, []);
+
+
+
+    return <>
+        <h1>Here are the tasks</h1>
+        <button>Add new task</button>
+        {tasks && tasks.map(t => <div>
+            <h3>{t.title} ({t.status})</h3>
+        </div>)}
+        {!tasks && <div>Loading...</div>}
+        </>;
+}
+
+root.render(<TaskApplication />)
