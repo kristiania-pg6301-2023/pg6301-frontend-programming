2b68336377c488879069c601f6e9e2ced521a081
Johannes Brodwall <johannes@brodwall.com>
2023-09-05 15:44:14 +0200

    Example of custom middleware

--- server/server.js
+++ server/server.js
@@ -1,22 +1,38 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    console.log("Request logging by johannes", req.url);
+    req.brukernavn = "pg6301-student"
+    next();
+})
+
+const tasks = [
+    {
+        title: "implement first version of server",
+        status: "done"
+    },
+    {
+        title: "first version of client",
+        status: "done"
+    },
+    {
+        title: "Better dev ex with client and server",
+        status: "doing"
+    }
+]
 
 app.get("/api/todos", (req, res) => {
-    res.send([
-        {
-            title: "implement first version of server",
-            status: "done"
-        },
-        {
-            title: "first version of client",
-            status: "done"
-        },
-        {
-            title: "Better dev ex with client and server",
-            status: "doing"
-        }
-    ]);
+    console.log(req.brukernavn);
+    res.send(tasks);
+});
+app.post("/api/todos", (req, res) => {
+    const {title} = req.body;
+    console.log("body", req.body);
+    const newTask = {title, status: "todo"};
+    tasks.push(newTask);
 });
 
 app.use(express.static("../client/dist"));
