5e3aff79f58df8e742d18711798fcf13c2849697
Johannes Brodwall <johannes@brodwall.com>
2023-09-20 12:49:44 +0200

    Create top level package.json

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Kristiania React Task Application</h1>)
+root.render(<h1>Kristiania React Task Application with Express and Reload</h1>)
