c1978ea327a3064648a2db0c123c58c86ffd596f
Johannes Brodwall <johannes@brodwall.com>
2023-09-20 13:40:03 +0200

    Improvement: Can now update tasks
    * (But all tasks are updated, and all titles are set to "UPdated")

--- client/index.jsx
+++ client/index.jsx
@@ -3,6 +3,27 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+function TaskListEntry({task}) {
+    async function handleUpdate() {
+        await fetch("/api/tasks", {
+            method: "PUT",
+            body: JSON.stringify({
+                title: "new title"
+            }),
+            headers: {
+                "content-type": "application/json"
+            }
+        });
+
+    }
+
+    return <p>
+        <input value={task.title} />
+        <br/>
+        <button onClick={handleUpdate}>Update</button>
+    </p>;
+}
+
 function TaskApplication() {
     const [tasks, setTasks] = useState([]);
 
@@ -18,7 +39,7 @@ function TaskApplication() {
 
     return <>
         <h1>Kristiania React Task Application on Heroku</h1>
-        {tasks.map(t => <div>{t.title}</div>)}
+        {tasks.map(t => <TaskListEntry task={t} />)}
     </>;
 }
 
--- server/server.js
+++ server/server.js
@@ -2,22 +2,31 @@ import express from "express";
 
 const app = express();
 
+const TASKS = [
+    {
+        title: "prepare lecture",
+        status: "done"
+    },
+    {
+        title: "give lecture",
+        status: "doing"
+    },
+    {
+        title: "deploy to heroku",
+        status: "todo"
+    }
+];
+
 app.get("/api/tasks", (req, res) => {
-    res.json([
-        {
-            title: "prepare lecture",
-            status: "done"
-        },
-        {
-            title: "give lecture",
-            status: "doing"
-        },
-        {
-            title: "deploy to heroku",
-            status: "todo"
-        }
-    ])
+    res.json(TASKS)
 })
+app.put("/api/tasks", (req, res) => {
+    for (const task of TASKS) {
+        task.title = "Updated";
+    }
+    res.end();
+})
+
 
 app.use(express.static("../client/dist"));
 
