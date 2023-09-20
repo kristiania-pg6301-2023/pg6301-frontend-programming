70e58d94140732c823aaba7f85422a5caba7af71
Johannes Brodwall <johannes@brodwall.com>
2023-09-20 13:29:15 +0200

    Load tasks from server

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Kristiania React Task Application on Heroku</h1>)
+
+function TaskApplication() {
+    const [tasks, setTasks] = useState([]);
+
+    async function loadTasks() {
+        const res = await fetch("/api/tasks");
+        setTasks(await res.json());
+    }
+
+    useEffect(() => {
+        loadTasks();
+    }, []);
+
+
+    return <>
+        <h1>Kristiania React Task Application on Heroku</h1>
+        {tasks.map(t => <div>{t.title}</div>)}
+    </>;
+}
+
+root.render(<TaskApplication/>)
