8dcbbc7eb73c6497ccfb717f416d749906938174
Johannes Brodwall <johannes@brodwall.com>
2023-09-26 11:52:47 +0200

    Implement /api/movies

--- server/server.js
+++ server/server.js
@@ -2,6 +2,19 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/movies", (req, res) => {
+  res.json([
+    {
+      id: 1,
+      title: "Oppenheimer from server",
+    },
+    {
+      id: 2,
+      title: "Barbie from server",
+    },
+  ]);
+});
+
 app.use(express.static("../client/dist"));
 
 app.listen(process.env.PORT || 3000);
