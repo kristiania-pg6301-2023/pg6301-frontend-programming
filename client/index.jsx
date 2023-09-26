60ded5744b2d6975b1dd8af9812d525ae7cbee2a
Johannes Brodwall <johannes@brodwall.com>
2023-09-26 10:36:53 +0200

    Movies application structure

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {MoviesApplication} from "./moviesApplication";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(<h1>React Movies application in Express with refresh</h1>)
+root.render(<MoviesApplication/>)
---
+++ client/moviesApplication.jsx
@@ -0,0 +1,20 @@
+import {BrowserRouter, Link} from "react-router-dom";
+import React from "react";
+
+export function MoviesApplication() {
+    return <BrowserRouter>
+        <header>
+            <h1>Kristiania movies</h1>
+        </header>
+        <nav>
+            <Link to={"/"}>Front page</Link>
+        </nav>
+        <main>
+            <ul>
+                <li><Link to={"/movies"}>List movies</Link></li>
+                <li><Link to={"/movies/new"}>Add a movie</Link></li>
+            </ul>
+        </main>
+        <footer>By Johannes Brodwall with 💚</footer>
+    </BrowserRouter>;
+}
