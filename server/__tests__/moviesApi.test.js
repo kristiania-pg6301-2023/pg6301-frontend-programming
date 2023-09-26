b36f7622244d6ba680563784b069ce0f6a551049
Johannes Brodwall <johannes@brodwall.com>
2023-09-26 12:04:33 +0200

    Passing api test with supertest

--- server/__tests__/moviesApi.test.js
+++ server/__tests__/moviesApi.test.js
@@ -1,3 +1,23 @@
+import request from "supertest";
+import express from "express";
+import { moviesApi } from "../moviesApi.js";
+
+const app = express();
+app.use(moviesApi);
+
+let expectedJson = [
+  {
+    id: 1,
+    title: "Oppenheimer from route",
+  },
+  {
+    id: 2,
+    title: "Barbie from route",
+  },
+];
+
 describe("movies api", () => {
-  it("return movies", () => {});
+  it("return movies", async () => {
+    await request(app).get("").expect(expectedJson);
+  });
 });
