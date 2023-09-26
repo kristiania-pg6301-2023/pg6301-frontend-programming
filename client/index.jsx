66cbd007efeab5a501d0df2d8a28cc23e35572d7
Johannes Brodwall <johannes@brodwall.com>
2023-09-26 10:33:21 +0200

    change parcel serve to parcel watch to enable HMR (hot module reloading)

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("app"));
-root.render(<h1>React Movies application</h1>)
+root.render(<h1>React Movies application in Express with refresh</h1>)
