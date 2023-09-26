fd956e7e85fee3271d570655cce8b558a1f4c089
Johannes Brodwall <johannes@brodwall.com>
2023-09-26 10:41:08 +0200

    Perfect styling

--- client/index.jsx
+++ client/index.jsx
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import {MoviesApplication} from "./moviesApplication";
 
+import "./application.css"
+
 const root = ReactDOM.createRoot(document.getElementById("app"));
 root.render(<MoviesApplication/>)
