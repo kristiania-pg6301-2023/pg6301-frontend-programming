d60d0f03de37d249f67f7a76eda27543bdf3e3cc
Johannes Brodwall <johannes@brodwall.com>
2023-09-27 12:46:37 +0200

    Update server test with supertest

--- server/__tests__/moviesApi.test.js
+++ server/__tests__/moviesApi.test.js
@@ -1,23 +1,19 @@
 import request from "supertest";
 import express from "express";
 import { moviesApi } from "../moviesApi.js";
+import bodyParser from "body-parser";
 
 const app = express();
+app.use(bodyParser.json());
 app.use(moviesApi);
 
-let expectedJson = [
-  {
-    id: 1,
-    title: "Oppenheimer from route",
-  },
-  {
-    id: 2,
-    title: "Barbie from route",
-  },
-];
-
 describe("movies api", () => {
-  it("return movies", async () => {
-    await request(app).get("").expect(expectedJson);
+  it("lets the user add a new movie", async () => {
+    const title = "My New Movie added at " + new Date();
+
+    await request(app).post("").send({ title }).expect(204);
+    const res = await request(app).get("");
+    expect(res.status).toBe(200);
+    expect(res.body.map((m) => m.title)).toContain(title);
   });
 });
--- server/moviesApi.js
+++ server/moviesApi.js
@@ -1,15 +1,23 @@
 import express from "express";
 
 export const moviesApi = express.Router();
+const MOVIES = [
+  {
+    id: 1,
+    title: "Oppenheimer from route",
+  },
+  {
+    id: 2,
+    title: "Barbie from route",
+  },
+];
+
 moviesApi.get("", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      title: "Oppenheimer from route",
-    },
-    {
-      id: 2,
-      title: "Barbie from route",
-    },
-  ]);
+  res.json(MOVIES);
+});
+
+moviesApi.post("", (req, res) => {
+  const { title } = req.body;
+  MOVIES.push({ id: MOVIES.length + 1, title });
+  res.sendStatus(204);
 });
