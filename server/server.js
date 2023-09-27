8c7667149aebc725cc0443d6c4909034c1d2bba1
Johannes Brodwall <johannes@brodwall.com>
2023-09-27 13:52:18 +0200

    add bodyParser on server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { moviesApi } from "./moviesApi.js";
+import bodyParser from "body-parser";
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use("/api/movies", moviesApi);
 app.use(express.static("../client/dist"));
 
