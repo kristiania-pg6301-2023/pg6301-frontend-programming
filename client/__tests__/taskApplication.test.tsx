28f3b7927fabb95352c1bdd2d44d8ce973429d2e
Johannes Brodwall <johannes@brodwall.com>
2023-10-05 00:31:30 +0200

    Make test pass!

--- client/__tests__/taskApplication.test.tsx
+++ client/__tests__/taskApplication.test.tsx
@@ -1,11 +1,14 @@
 import { TaskRoutes } from "../taskApplication";
 import React from "react";
 import renderer from "react-test-renderer";
+import { MemoryRouter } from "react-router-dom";
 
 describe("task application", () => {
   it("displays task list", () => {
     const component = renderer.create(
-      <TaskRoutes fetchTasks={() => []} onAddTask={() => {}} />,
+      <MemoryRouter initialEntries={["/tasks"]}>
+        <TaskRoutes fetchTasks={() => []} onAddTask={() => {}} />,
+      </MemoryRouter>,
     );
     expect(component).toMatchSnapshot();
   });
