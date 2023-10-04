1069449301c5f0a0673d22777b07c14770e0b21d
Johannes Brodwall <johannes@brodwall.com>
2023-10-05 00:40:00 +0200

    Add test for AddTaskForm

--- client/__tests__/taskApplication.test.tsx
+++ client/__tests__/taskApplication.test.tsx
@@ -1,6 +1,6 @@
 import { TaskRoutes } from "../taskApplication";
 import React from "react";
-import renderer from "react-test-renderer";
+import renderer, { act } from "react-test-renderer";
 import { MemoryRouter } from "react-router-dom";
 
 describe("task application", () => {
@@ -11,5 +11,25 @@ describe("task application", () => {
       </MemoryRouter>,
     );
     expect(component).toMatchSnapshot();
+  });
+  it("submits a new task", () => {
+    const onAddTask = jest.fn();
+    const component = renderer.create(
+      <MemoryRouter initialEntries={["/tasks/new"]}>
+        <TaskRoutes fetchTasks={() => []} onAddTask={onAddTask} />,
+      </MemoryRouter>,
+    );
+    const title = "Expected Task Title";
+    act(() => {
+      component.root
+        .findByType("input")
+        .props.onChange({ target: { value: title } });
+    });
+    act(() => {
+      component!.root.findByType("form").props.onSubmit({
+        preventDefault: () => {},
+      });
+    });
+    expect(onAddTask).toHaveBeenCalledWith({ title });
   });
 });
