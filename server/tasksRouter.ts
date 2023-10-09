af2dbeb1aa07eefe386c9a60574390e78a9d6528
Johannes Brodwall <johannes@brodwall.com>
2023-10-09 02:16:31 +0200

    Refactor tasksRouter to use express.Router

--- server/tasksRouter.ts
+++ server/tasksRouter.ts
@@ -1,26 +1,22 @@
-import { NextFunction, Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 export const TASKS = [
-  { _id: 1, title: "task one from the server" },
-  { _id: 2, title: "task two from the server" },
+  { _id: 1, title: "task one from route" },
+  { _id: 2, title: "task two from route" },
 ];
 
-export const tasksRouter = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  if (req.path === "/api/tasks" && req.method === "GET") {
-    setTimeout(() => {
-      return res.send(TASKS);
-    }, 500);
-  } else if (req.path === "/api/tasks" && req.method === "POST") {
-    const { title } = req.body;
-    setTimeout(() => {
-      TASKS.push({ _id: TASKS.length + 1, title });
-      res.sendStatus(201);
-    }, 500);
-  } else {
-    next();
-  }
-};
+export const tasksRouter = express.Router();
+
+tasksRouter.get("/api/tasks", (req, res) => {
+  setTimeout(() => {
+    return res.send(TASKS);
+  }, 500);
+});
+
+tasksRouter.post("/api/tasks", (req, res) => {
+  const { title } = req.body;
+  setTimeout(() => {
+    TASKS.push({ _id: TASKS.length + 1, title });
+    res.sendStatus(201);
+  }, 500);
+});
