56d36673091a617cc56b086445f661e76498b4fa
Johannes Brodwall <johannes@brodwall.com>
2023-10-09 02:44:59 +0200

    create middleware for movies router

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,6 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
-import { MongoClient } from "mongodb";
+import { Db, MongoClient } from "mongodb";
 import * as path from "path";
 import { tasksRouter } from "./tasksRouter";
 
@@ -9,23 +9,36 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use(tasksRouter);
-app.use(express.static("../client/dist"));
-app.use((req, res, next) => {
-  if (req.method === "GET" && !req.path.startsWith("/api")) {
-    res.sendFile(path.resolve("../client/dist/index.html"));
-  } else {
-    next();
-  }
+const client = new MongoClient(process.env.MONGODB_URL!);
+client.connect().then(async (connection) => {
+  const db = connection.db("sample_mflix");
+  app.use(createMoviesRouter(db));
+  app.use(tasksRouter);
+  app.use(express.static("../client/dist"));
+  app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+      res.sendFile(path.resolve("../client/dist/index.html"));
+    } else {
+      next();
+    }
+  });
 });
 
 app.listen(3000);
 
-const client = new MongoClient(process.env.MONGODB_URL!);
-client.connect().then(async (connection) => {
-  const db = connection.db("sample_mflix");
-  const movies = await db
-    .collection("movies")
-    .distinct("year", { countries: "Norway" });
-  console.log(movies);
-});
+function createMoviesRouter(db: Db) {
+  const router = async (req: Request, res: Response, next: NextFunction) => {
+    if (req.path.startsWith("/api/movies")) {
+      if (req.path === "/api/movies/parameters") {
+        const years = await db
+          .collection("movies")
+          .distinct("year", { countries: "Norway" });
+        return res.send({ years });
+      }
+      res.sendStatus(404);
+    } else {
+      next();
+    }
+  };
+  return router;
+}
