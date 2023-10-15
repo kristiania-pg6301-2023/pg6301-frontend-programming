23de094f32b4695085b0e87571c8beeaf9fbf304
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 15:41:23 +0200

    login should fail when user is not logged in

--- server/loginRouter.js
+++ server/loginRouter.js
@@ -3,7 +3,5 @@ import express from "express";
 export const loginRouter = express.Router();
 
 loginRouter.get("", (req, res) => {
-  res.send({
-    username: "Johannes from server",
-  });
+  res.sendStatus(401);
 });
