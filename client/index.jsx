da9a07f08de83083a05e90ceae8544fea0f6f885
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 15:22:23 +0200

    run express and react concurrently

--- client/index.jsx
+++ client/index.jsx
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom/client"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>User database</h1>)
+root.render(<h1>User database from react</h1>)
