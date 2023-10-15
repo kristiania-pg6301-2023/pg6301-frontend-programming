79cb49e334e4e41d808c79ea03705c0305f09aa1
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 15:53:56 +0200

    repetion: server POST handler

--- server/loginRouter.js
+++ server/loginRouter.js
@@ -5,3 +5,8 @@ export const loginRouter = express.Router();
 loginRouter.get("", (req, res) => {
   res.sendStatus(401);
 });
+
+loginRouter.post("", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(204);
+});
