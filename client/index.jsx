722d4206c229b3465707baf22dea7f7d3d8913d0
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 15:24:55 +0200

    Front-end application structure

--- client/index.jsx
+++ client/index.jsx
@@ -1,5 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>User database from react</h1>)
+
+function Application() {
+  return <>
+    <header><h1>User database</h1></header>
+    <nav>
+      <Link to={"/"}>Front page</Link>
+      <Link to={"/login"}>Log in</Link>
+    </nav>
+    <main>
+      <Routes>
+        <Route path={"/"} element={<h2>Front page</h2>} />
+        <Route path={"*"} element={<h2>Not Found</h2>} />
+      </Routes>
+    </main>
+    <footer>Lecture 9: Open ID Connect</footer>
+    </>;
+}
+
+root.render(<BrowserRouter><Application /></BrowserRouter>)
