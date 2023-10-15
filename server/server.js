d8ddd77b522795972437e753025bd0ce5f8b368a
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 15:39:49 +0200

    send username from server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
 import express from "express";
 
 const app = express();
+
+const loginRouter = express.Router();
+
+loginRouter.get("", (req, res) => {
+  res.send({
+    username: "Johannes from server",
+  });
+});
+
+app.use("/api/login", loginRouter);
+
 app.use(express.static("../client/dist"));
 app.listen(3000);
