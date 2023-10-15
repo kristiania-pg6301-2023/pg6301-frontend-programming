30f060748f05510960daf5094476c03984297356
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 17:56:06 +0200

    set req.user in middleware

--- server/loginRouter.js
+++ server/loginRouter.js
@@ -1,35 +1,10 @@
 import express from "express";
 
-const OPENID_DISCOVERY_URL =
-  "https://accounts.google.com/.well-known/openid-configuration";
-
-async function fetchJson(url) {
-  const res = await fetch(url);
-  if (!res.ok) {
-    throw new Error("Can't fetch " + url);
-  }
-  return await res.json();
-}
-
 export const loginRouter = express.Router();
 
 loginRouter.get("", async (req, res) => {
-  const { username, access_token } = req.signedCookies;
-  if (username) {
-    res.send({ username });
-  } else if (access_token) {
-    const discoveryDocument = await fetchJson(OPENID_DISCOVERY_URL);
-    const { userinfo_endpoint } = discoveryDocument;
-    const userinfoRes = await fetch(userinfo_endpoint, {
-      headers: {
-        authorization: `Bearer ${access_token}`,
-      },
-    });
-    const userinfo = await userinfoRes.json();
-    res.send({
-      ...userinfo,
-      username: userinfo.given_name + " " + userinfo.family_name,
-    });
+  if (req.user) {
+    res.send(req.user);
   } else {
     res.sendStatus(401);
   }
--- server/server.js
+++ server/server.js
@@ -4,11 +4,42 @@ import { loginRouter } from "./loginRouter.js";
 import dotenv from "dotenv";
 import * as path from "path";
 
+const OPENID_DISCOVERY_URL =
+  "https://accounts.google.com/.well-known/openid-configuration";
+
+async function fetchJson(url, params) {
+  const res = await fetch(url, params);
+  if (!res.ok) {
+    throw new Error("Can't fetch " + url);
+  }
+  return await res.json();
+}
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_PARSER_SECRET));
+
+app.use(async (req, res, next) => {
+  const { username, access_token } = req.signedCookies;
+  if (access_token) {
+    const discoveryDocument = await fetchJson(OPENID_DISCOVERY_URL);
+    const { userinfo_endpoint } = discoveryDocument;
+    const user = await fetchJson(userinfo_endpoint, {
+      headers: {
+        authorization: `Bearer ${access_token}`,
+      },
+    });
+    const { given_name, family_name } = user;
+    const username = `${given_name} ${family_name}`;
+    req.user = { ...user, username };
+  } else if (username) {
+    req.user = { username: username };
+  }
+
+  next();
+});
 
 app.use("/api/login", loginRouter);
 app.use(express.static("../client/dist"));
