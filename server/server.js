9a8df6518b1ff4ee838835a26dc7a08f7e90133d
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 19:00:37 +0200

    server.js hint for dealing with BrowserRouter

--- server/server.js
+++ server/server.js
@@ -1,5 +1,18 @@
 import express from "express";
+import * as path from "path";
 
 const app = express();
 app.use(express.static("../client/dist"));
+
+/*
+The following code will fix 404 errors when refreshing with <BrowserRouter />
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
+ */
+
 app.listen(3000);
