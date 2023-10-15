e593faf333842c561a2dea2a964f57ec7a54c861
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 19:12:48 +0200

    <MoviesList /> implementation

--- client/components/application/applicationRoutes.jsx
+++ client/components/application/applicationRoutes.jsx
@@ -2,12 +2,38 @@ import { Route, Routes } from "react-router-dom";
 import { FrontPage } from "./frontPage";
 import { MoviesList } from "../movies/moviesList";
 import { AddMovieForm } from "../movies/addMovieForm";
+import { useEffect, useState } from "react";
+
+const MOVIES = [
+  {
+    _id: "1",
+    title: "Barbie",
+    year: 2023,
+    countries: ["USA"],
+  },
+  {
+    _id: "2",
+    title: "Oppenheimer",
+    year: 2023,
+    countries: ["USA"],
+  },
+];
 
 export function ApplicationRoutes() {
+  const [movies, setMovies] = useState([]);
+
+  async function loadMovies() {
+    setMovies(MOVIES);
+  }
+
+  useEffect(() => {
+    loadMovies();
+  }, []);
+
   return (
     <Routes>
       <Route path={"/"} element={<FrontPage />} />
-      <Route path={"/movies"} element={<MoviesList />} />
+      <Route path={"/movies"} element={<MoviesList movies={movies} />} />
       <Route path={"/movies/new"} element={<AddMovieForm />} />
       <Route path={"*"} element={<h2>Not Found</h2>} />
     </Routes>
--- client/components/movies/moviesList.jsx
+++ client/components/movies/moviesList.jsx
@@ -1,3 +1,25 @@
-export function MoviesList() {
-  return <h2>Movies List</h2>;
+function MovieEntry({ movie }) {
+  const { title, year, countries, plot } = movie;
+  return (
+    <>
+      <h3>
+        {title} ({year})
+      </h3>
+      <div>
+        <p>Made in: {countries.join(", ")}</p>
+        <div>{plot}</div>
+      </div>
+    </>
+  );
+}
+
+export function MoviesList({ movies }) {
+  return (
+    <>
+      <h2>Movies List</h2>
+      {movies.map((m) => (
+        <MovieEntry key={m._id} movie={m} />
+      ))}
+    </>
+  );
 }
