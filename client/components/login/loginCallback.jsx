7ff67c956825cf2f6282ee46995ac22dc02d0a0d
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 17:31:34 +0200

    try to post the access token to the server

--- client/components/login/loginCallback.jsx
+++ client/components/login/loginCallback.jsx
@@ -1,11 +1,30 @@
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { LoginContext } from "./loginButton";
 
 export function LoginCallback() {
+  const { reload } = useContext(LoginContext);
+  const navigate = useNavigate();
   const callbackParameters = Object.fromEntries(
-    new URLSearchParams(window.location.hash),
+    new URLSearchParams(window.location.hash.substring(1)),
   );
 
-  async function handleCallback() {}
+  async function handleCallback() {
+    const { access_token } = callbackParameters;
+    const res = await fetch("/api/login/accessToken", {
+      method: "POST",
+      body: JSON.stringify({ access_token }),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    if (!res.ok) {
+      console.error(await res.text());
+      throw new Error("Failed to POST access token");
+    }
+    await reload();
+    navigate("/");
+  }
 
   useEffect(() => {
     handleCallback();
