8805141931e4e2cb9239f1e8ba96dd9f0e0c04ca
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 19:31:22 +0200

    Add dummy code for MongoDB

--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 import express from "express";
-import * as path from "path";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 app.use(express.static("../client/dist"));
@@ -14,5 +17,18 @@ app.use((req, res, next) => {
   }
 });
  */
+
+// For more, see https://github.com/kristiania-pg6301-2023/pg6301-frontend-programming#reading-data-from-mongodb
+async function handleMongoDbConnect(connection) {
+  const moviesCollection = connection.db("sample_mflix").collection("movies");
+  const distinctYearsFromMovies = await moviesCollection.distinct("year", {
+    countries: "Norway",
+  });
+  console.log(distinctYearsFromMovies);
+}
+
+if (process.env.MONGODB_URL) {
+  new MongoClient(process.env.MONGODB_URL).connect().then(handleMongoDbConnect);
+}
 
 app.listen(3000);
