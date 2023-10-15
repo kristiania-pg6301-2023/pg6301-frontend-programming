8188d7e51e356f8df44433384fec8a369783d696
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 15:25:20 +0200

    Remember prettier!

--- client/index.jsx
+++ client/index.jsx
@@ -1,24 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom/client"
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Application() {
-  return <>
-    <header><h1>User database</h1></header>
-    <nav>
-      <Link to={"/"}>Front page</Link>
-      <Link to={"/login"}>Log in</Link>
-    </nav>
-    <main>
-      <Routes>
-        <Route path={"/"} element={<h2>Front page</h2>} />
-        <Route path={"*"} element={<h2>Not Found</h2>} />
-      </Routes>
-    </main>
-    <footer>Lecture 9: Open ID Connect</footer>
-    </>;
+  return (
+    <>
+      <header>
+        <h1>User database</h1>
+      </header>
+      <nav>
+        <Link to={"/"}>Front page</Link>
+        <Link to={"/login"}>Log in</Link>
+      </nav>
+      <main>
+        <Routes>
+          <Route path={"/"} element={<h2>Front page</h2>} />
+          <Route path={"*"} element={<h2>Not Found</h2>} />
+        </Routes>
+      </main>
+      <footer>Lecture 9: Open ID Connect</footer>
+    </>
+  );
 }
 
-root.render(<BrowserRouter><Application /></BrowserRouter>)
+root.render(
+  <BrowserRouter>
+    <Application />
+  </BrowserRouter>,
+);
