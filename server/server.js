a30f78fdcb2fb19315fb8d124356d102c744d4b4
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 15:45:17 +0200

    repetition: fetch default resource

--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { loginRouter } from "./loginRouter.js";
+import * as path from "path";
 
 const app = express();
 
 app.use("/api/login", loginRouter);
 app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+  res.sendFile(path.resolve("../client/dist/index.html"));
+});
 app.listen(3000);
