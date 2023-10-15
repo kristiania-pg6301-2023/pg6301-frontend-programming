7069ca6bd199c025f4c5e68a9da2a8904758b6fb
Johannes Brodwall <johannes@brodwall.com>
2023-10-15 19:04:43 +0200

    add application.css for some simple but effective styling

--- client/components/application/application.jsx
+++ client/components/application/application.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { ApplicationRoutes } from "./applicationRoutes";
 
+import "./application.css";
+
 export function Application() {
   return (
     <>
