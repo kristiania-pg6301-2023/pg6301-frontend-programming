a282cfd0bc30431a820d90a5d0e0063cf978563e
Johannes Brodwall <johannes@brodwall.com>
2023-10-18 12:41:31 +0200

    login page form with submit

--- client/components/login/loginPage.jsx
+++ client/components/login/loginPage.jsx
@@ -1,5 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function LoginPage() {
-  return <h2>Login page</h2>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login page</h2>
+      <div>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <button>Log in</button>
+      </div>
+      <pre>{JSON.stringify({ username, password })}</pre>
+    </form>
+  );
 }
