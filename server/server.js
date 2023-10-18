171a44c1c1c1fa0bc6691deabddb26fab2e5f2d0
Johannes Brodwall <johannes@brodwall.com>
2023-10-18 14:03:25 +0200

    server fetches userinfo from discovery document's userinfo_endpoint

--- server/server.js
+++ server/server.js
@@ -9,9 +9,29 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-app.use((req, res, next) => {
-  const { username } = req.signedCookies;
-  req.user = { username };
+const DISCOVERY_URL =
+  "https://accounts.google.com/.well-known/openid-configuration";
+
+app.use(async (req, res, next) => {
+  const { username, access_token } = req.signedCookies;
+  if (access_token) {
+    const res = await fetch(DISCOVERY_URL);
+    const discoveryDoc = await res.json();
+
+    const userinfoRes = await fetch(discoveryDoc.userinfo_endpoint, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    if (!userinfoRes.ok) {
+      throw new Error("The error was " + userinfoRes.statusText);
+    }
+    const userinfo = await userinfoRes.json();
+
+    req.user = { ...userinfo, username: userinfo.email };
+  } else {
+    req.user = { username };
+  }
   next();
 });
 
