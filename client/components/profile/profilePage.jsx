568635c79a626752a3f4748d950332ea19095109
Johannes Brodwall <johannes@brodwall.com>
2023-10-18 13:30:29 +0200

    logout button in profile page

--- client/components/profile/profilePage.jsx
+++ client/components/profile/profilePage.jsx
@@ -3,10 +3,25 @@ import { LoginContext } from "../login/loginContext";
 
 export function ProfilePage() {
   const { username } = useContext(LoginContext);
+
+  async function handleSubmitLogout(e) {
+    e.preventDefault();
+    const res = await fetch("/api/login", {
+      method: "DELETE",
+    });
+    if (!res.ok) {
+      throw new Error("Failed to log out " + res.statusText);
+    }
+  }
+
   return (
     <>
       <h2>Profile page</h2>
       <div>Username: {username}</div>
+
+      <form onSubmit={handleSubmitLogout}>
+        <button>Log out</button>
+      </form>
     </>
   );
 }
