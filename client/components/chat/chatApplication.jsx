f82f99a7403fd2123ba610b99fe403b52ba2592a
Johannes Brodwall <johannes@brodwall.com>
2023-10-22 11:51:04 +0200

    chat application logic (client side only)

--- client/components/chat/chatApplication.jsx
+++ client/components/chat/chatApplication.jsx
@@ -1,67 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./application.css";
 
 export function ChatApplication() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setMessages((current) => [...current, newMessage]);
+    setNewMessage("");
+  }
+
   return (
     <>
       <header>
         <h2>Kristiania Chat!</h2>
       </header>
       <main>
-        <div>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. At,
-          molestias!
-        </div>
-        <div>
-          Accusamus, culpa cumque eligendi facere fugit molestias quas quos
-          repudiandae?
-        </div>
-        <div>
-          Exercitationem facilis modi provident, qui tenetur voluptates!
-          Consequatur, corporis, cumque!
-        </div>
-        <div>
-          A ab adipisci atque eligendi labore necessitatibus, nobis quia
-          repudiandae.
-        </div>
-        <div>
-          Cumque delectus doloribus provident quas quibusdam rem veritatis!
-          Totam, voluptatibus.
-        </div>
-        <div>
-          Aut enim impedit ipsam laboriosam odit possimus quas repudiandae
-          voluptatum.
-        </div>
-        <div>
-          Assumenda, eligendi excepturi exercitationem fugiat harum hic magni
-          possimus quis?
-        </div>
-        <div>
-          Accusantium cum, expedita impedit itaque libero natus quidem
-          reiciendis voluptatum.
-        </div>
-        <div>
-          Facere inventore nulla officia omnis quasi, quia? Corporis,
-          recusandae, soluta.
-        </div>
-        <div>
-          Ab amet autem corporis delectus iure laborum qui quibusdam? Nulla.
-        </div>
-        <div>
-          Aliquam enim explicabo fugiat molestiae quis quo sit suscipit vitae.
-        </div>
-        <div>
-          Alias, dolor eius fuga natus recusandae rerum tempora. Ea, nam?
-        </div>
-        <div>Ad animi eos natus quia quis. Cupiditate in natus sit?</div>
-        <div>
-          Ad, alias aliquam debitis ducimus explicabo natus porro quam totam?
-        </div>
+        {messages.map((message) => (
+          <div>{message}</div>
+        ))}
       </main>
       <footer>
-        <form>
-          <input autoFocus />
+        <form onSubmit={handleSubmit}>
+          <input
+            autoFocus
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
           <button>Chat</button>
         </form>
       </footer>
