e4afb503aa6014110625827ffd07daaa13b8993f
Johannes Brodwall <johannes@brodwall.com>
2023-10-22 11:41:43 +0200

    client/index.html and index.jsx

---
+++ client/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function ChatApplication() {
+  return <h2>Kristiania Chat!</h2>;
+}
+
+root.render(<ChatApplication />);
