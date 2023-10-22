860a9954acf86a414c9470a1ca7c84608ef78650
Johannes Brodwall <johannes@brodwall.com>
2023-10-22 11:39:21 +0200

    setup server.js

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import * as path from "path";
+
+const app = express();
+app.use(express.static("../client/dist"));
+app.use((req, res, next) => {
+  if (req.method === "GET" && !req.path.startsWith("/api")) {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
+
+app.listen(process.env.PORT || 3000);
