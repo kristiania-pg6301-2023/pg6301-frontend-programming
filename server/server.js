2a46ab2fd1e25a414bf5578d7088e96424a24340
Johannes Brodwall <johannes@brodwall.com>
2023-10-25 13:22:19 +0200

    server echos all messages from client

--- server/server.js
+++ server/server.js
@@ -18,8 +18,19 @@ const wsServer = new WebSocketServer({ noServer: true });
 
 const server = app.listen(process.env.PORT || 3000);
 
+const sockets = [];
+
 server.on("upgrade", (req, socket, head) => {
   wsServer.handleUpgrade(req, socket, head, (socket) => {
+    sockets.push(socket);
     socket.send("Hello from the server");
+
+    socket.on("message", (message) => {
+      console.log(message.toString());
+
+      for (const s of sockets) {
+        s.send(message.toString());
+      }
+    });
   });
 });
