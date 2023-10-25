1f2f6f3192d07868baa771bc6fe64b4b40635752
Johannes Brodwall <johannes@brodwall.com>
2023-10-25 12:41:36 +0200

    make the server work

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+import * as path from "path";
+
+const app = express();
+
+app.use(express.static("../client/dist/"));
+
+app.use((req, res, next) => {
+  if (req.method === "GET") {
+    res.sendFile(path.resolve("../client/dist/index.html"));
+  } else {
+    next();
+  }
+});
+
+app.listen(process.env.PORT || 3000);
