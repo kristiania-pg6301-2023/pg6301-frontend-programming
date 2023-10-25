2002309eed5c2eff532493f5fded668f799cb822
Johannes Brodwall <johannes@brodwall.com>
2023-10-25 12:53:02 +0200

    we are talking to ourselves

--- client/components/application/application.jsx
+++ client/components/application/application.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+
 export function Application() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setMessages((current) => [...current, newMessage]);
+    setNewMessage("");
+  }
+
   return (
     <>
       <header>
@@ -6,79 +18,19 @@ export function Application() {
       </header>
 
       <main>
-        <li>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis,
-          possimus!
-        </li>
-        <li>
-          A blanditiis commodi cumque esse molestiae molestias quasi quos
-          voluptatem.
-        </li>
-        <li>
-          Cum dicta doloremque eius eum expedita hic illum libero provident.
-        </li>
-        <li>Amet commodi est iste iusto minima nihil odio. In, temporibus.</li>
-        <li>
-          Aliquam animi deserunt ex magni mollitia obcaecati qui tempora,
-          voluptatem!
-        </li>
-        <li>
-          Accusamus consectetur deleniti nobis numquam officiis porro quaerat
-          quas quasi?
-        </li>
-        <li>
-          A dolor facere nam numquam quibusdam quisquam quo sunt. Delectus?
-        </li>
-        <li>
-          Doloremque fugiat ipsam iste libero maiores modi non nostrum
-          provident?
-        </li>
-        <li>
-          Asperiores dignissimos illum ipsum nam quibusdam rem, rerum sapiente
-          vero?
-        </li>
-        <li>
-          Eveniet fugit ipsam molestiae. Eaque molestiae quis recusandae soluta
-          tenetur!
-        </li>
-        <li>
-          Corporis eum exercitationem non, nulla numquam quibusdam tempore.
-          Consectetur, impedit.
-        </li>
-        <li>
-          Architecto atque dignissimos provident quae? Aliquam illo impedit modi
-          ratione!
-        </li>
-        <li>
-          At eligendi illum in minus quisquam quo repellendus sunt veniam.
-        </li>
-        <li>Aut beatae dolore ex iste nemo neque sed tenetur voluptatem.</li>
-        <li>
-          Alias cupiditate dignissimos eius fugit ipsam maxime odio quis vero!
-        </li>
-        <li>
-          Asperiores aspernatur dolor nulla, obcaecati omnis quae qui
-          reprehenderit veniam?
-        </li>
-        <li>
-          Alias facilis in, necessitatibus qui repudiandae saepe voluptas.
-          Aliquam, commodi?
-        </li>
-        <li>
-          Aliquid consectetur expedita itaque porro quidem sint sunt veniam
-          vero?
-        </li>
-        <li>
-          Accusamus culpa fuga ipsam natus numquam rerum. Excepturi, nulla quis.
-        </li>
-        <li>
-          Adipisci commodi in molestiae perferendis sapiente voluptate
-          voluptatem. Minus, tenetur.
-        </li>
+        {messages.map((message) => (
+          <li>{message}</li>
+        ))}
       </main>
       <footer>
-        <input autoFocus />
-        <button>Chat</button>
+        <form onSubmit={handleSubmit}>
+          <input
+            autoFocus
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
+          <button>Chat</button>
+        </form>
       </footer>
     </>
   );
