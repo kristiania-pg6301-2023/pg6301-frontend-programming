0b9ae5d242f3c476815a9092cf2859ca7341abad
Johannes Brodwall <johannes@brodwall.com>
2023-10-30 17:53:46 +0100

    start handling callback

--- client/components/login/loginCallback.jsx
+++ client/components/login/loginCallback.jsx
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react";
+
 export function LoginCallback({ applicationConfig }) {
-  return <div>Login callback</div>;
+  const [debug, setDebug] = useState();
+
+  async function handleCallback() {
+    const hash = Object.fromEntries(new URLSearchParams(window.location.hash));
+    setDebug(hash);
+  }
+  useEffect(() => {
+    handleCallback();
+  }, []);
+
+
+  return <>
+    <div>Login callback</div>
+    <pre>{JSON.stringify(debug, null, 2)}</pre>
+  </>;
 }
