6b3ebf327ba5952f0bb26388822ae3e93d581cf2
Johannes Brodwall <johannes@brodwall.com>
2023-10-30 08:43:01 +0100

    fetch config from server

--- client/components/application/application.jsx
+++ client/components/application/application.jsx
@@ -1,3 +1,25 @@
+import { useEffect, useState } from "react";
+
+async function fetchJSON(path) {
+  const res = await fetch(path);
+  if (!res.ok) {
+    throw new Error(`Failed to fetch ${path}: ${res.statusText}`);
+  }
+  return await res.json();
+}
+
 export function Application() {
-    return <h1>Application</h1>;
+  async function loadConfig() {
+    setApplicationConfig(await fetchJSON("/api/config"));
+  }
+
+  const [applicationConfig, setApplicationConfig] = useState();
+  useEffect(() => {
+    loadConfig();
+  }, []);
+
+  if (!applicationConfig) {
+    return <div>Loading...</div>;
+  }
+  return <h1>Application</h1>;
 }
