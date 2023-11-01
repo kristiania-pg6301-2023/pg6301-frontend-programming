0795e7dfa9a72383d9c22fad6390128f23ae83b2
Johannes Brodwall <johannes@brodwall.com>
2023-11-01 13:25:32 +0100

    generate correct authorization_uri for ID-porten

--- client/components/login/loginButton.jsx
+++ client/components/login/loginButton.jsx
@@ -6,14 +6,25 @@ export function LoginButton({ applicationConfig }) {
     const { openid_configuration, client_id } = applicationConfig;
     const { authorization_endpoint } = await fetchJSON(openid_configuration);
     const redirect_uri = window.location.origin + "/login/callback";
+
+    const code_verifier = randomString(50);
+    window.sessionStorage.setItem("code_verifier", code_verifier);
+    const code_challenge = await sha256(code_verifier);
+    const state = randomString(50);
+    window.sessionStorage.setItem("state", state);
+
     setAuthorizationUrl(
       authorization_endpoint +
         "?" +
         new URLSearchParams({
-          response_type: "token",
-          scope: "email profile",
+          response_mode: "fragment",
+          response_type: "code",
+          scope: "openid profile",
           client_id,
           redirect_uri,
+          code_challenge,
+          code_challenge_method: "S256",
+          state,
         }),
     );
   }
@@ -29,4 +40,28 @@ export function LoginButton({ applicationConfig }) {
       <a href={authorizationUrl}>Log in</a>
     </div>
   );
+}
+
+function randomString(length) {
+  let result = "";
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let counter = 0;
+  while (counter < length) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    counter += 1;
+  }
+  return result;
+}
+
+export async function sha256(string) {
+  const binaryHash = await crypto.subtle.digest(
+    "SHA-256",
+    new TextEncoder("utf-8").encode(string),
+  );
+  return btoa(String.fromCharCode.apply(null, new Uint8Array(binaryHash)))
+    .split("=")[0]
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_");
 }
--- client/components/login/loginCallback.jsx
+++ client/components/login/loginCallback.jsx
@@ -1,25 +1,45 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export function LoginCallback({ onLogin }) {
+  const [error, setError] = useState();
+  const [errorDescription, setErrorDescription] = useState();
   async function handleCallback() {
-    const { access_token } = Object.fromEntries(
+    const hash = Object.fromEntries(
       new URLSearchParams(window.location.hash.substring(1)),
     );
-    const res = await fetch("/api/login", {
-      method: "POST",
-      body: JSON.stringify({ access_token }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    if (!res.ok) {
-      throw new Error("Login failed " + res.statusText);
+
+    const { access_token, error, error_description, state } = hash;
+    setError(error);
+    setErrorDescription(error_description);
+
+    if (state !== window.sessionStorage.getItem("state")) {
+      setError("Invalid state");
+      return;
     }
-    onLogin();
+
+    if (access_token) {
+      const res = await fetch("/api/login", {
+        method: "POST",
+        body: JSON.stringify({ access_token }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (!res.ok) {
+        throw new Error("Login failed " + res.statusText);
+      }
+      onLogin();
+    }
   }
   useEffect(() => {
     handleCallback();
   }, []);
 
-  return <div>Login callback</div>;
+  return (
+    <>
+      <div>Login callback</div>
+      {error && <h3>{error}</h3>}
+      {errorDescription && <p>{errorDescription}</p>}
+    </>
+  );
 }
